34f12ec34c4454c8efc9cdaec785d306537b053a
Syedailsa <159128442+Syedailsa@users.noreply.github.com>
2024-12-05 03:58:51 +0500

    upload

--- src/components/layout/hireme-dialog.tsx
+++ src/components/layout/hireme-dialog.tsx
@@ -27,7 +27,7 @@ const DialogDemo = () => {
               <div>
                 <h2 className="dialog-title text-blue">Get FREE consultation now!</h2>
                 <p className="dialog-description">
-                  Help's to build FAST, SECURE, and INTERACTIVE websites
+                  Help&apos;s to build FAST, SECURE, and INTERACTIVE websites
                 </p>                
               </div>
               <button className="close-button" onClick={toggleDialog}>
@@ -89,86 +89,3 @@ const DialogDemo = () => {
 export default DialogDemo;
 
 
-
-// import { Button } from "@/components/ui/button";
-// import {
-//   Dialog,
-//   DialogContent,
-//   DialogDescription,
-//   DialogFooter,
-//   DialogHeader,
-//   DialogTitle,
-//   DialogTrigger,
-// } from "@/components/ui/dialog";
-// import Contactform from "./contactform";
-// import Image from "next/image";
-
-// export function DialogDemo() {
-//   return (
-//     <Dialog>
-//       <DialogTrigger asChild>
-//         <Button className='hireMe'>
-//           Hire Me
-//         </Button>
-//       </DialogTrigger>
-//       <DialogContent className="dialog-content">
-//         <DialogHeader className="text-white">
-//           <DialogTitle className="text-blue">
-//             Get FREE consultation now !
-//           </DialogTitle>
-//           <DialogDescription className="">
-//             Help&apos;s to build FAST, SECURE and INTERACTIVE websites
-//           </DialogDescription>
-//         </DialogHeader>
-//         <div className="grid gap-4 text-white">
-//           <Contactform />
-//           <div>
-//             <div className="flex gap-2 justify-center items-center pt-2">
-              // <div className="w-6 sm:w-10">
-              //   <Image
-              //     src="/nextlogo.svg"
-              //     alt="Next.js Logo"
-              //     width={80}
-              //     height={20}
-              //   />
-              // </div>
-              // <div className="w-10">
-              //   <Image
-              //     src="/tailwindlogo.svg"
-              //     alt="Tailwind CSS Logo"
-              //     width={80}
-              //     height={20}
-              //   />
-              // </div>
-              // <div className="w-10">
-              //   <Image
-              //     src="/sanitylogo.svg"
-              //     alt="Sanity Logo"
-              //     width={80}
-              //     height={20}
-              //   />
-              // </div>
-              // <div className="w-10">
-              //   <Image
-              //     src="/tslogo.svg"
-              //     alt="TypeScript Logo"
-              //     width={80}
-              //     height={20}
-              //   />
-              // </div>
-              // <div className="w-10">
-              //   <Image
-              //     src="/auth.svg"
-              //     alt="NextAuth Logo"
-              //     width={80}
-              //     height={20}
-              //   />
-              // </div>
-//             </div>
-//           </div>
-//         </div>
-//         <DialogFooter className="text-white"></DialogFooter>
-//       </DialogContent>
-//     </Dialog>
-//   );
-// }
